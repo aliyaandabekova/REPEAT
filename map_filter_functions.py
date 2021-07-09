f0662d60c997d1690e25374f5eba9eabdd5c1184
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-07-09 15:45:09 +0300

    07.07

---
+++ map_filter_functions.py
@@ -0,0 +1,33 @@
+# # str1 = '                 fghj              '
+# # print(str1.split())
+# #
+# #
+# # print(list(range(10)))
+# # print(dict(hours=10))
+# #
+# # import random
+# # print(random.randint(1,10))
+#
+# # l1 = [1,2,3,4,5,6,7]
+# #
+# # def map1(elem):
+# #     result = elem % 2
+# #     return result
+# #
+# # l2 = map(map1,l1)
+# # print(list(l2))
+#
+#
+# import random
+# l1 =[]
+# for i in range(10000):
+#     l1.append(random.randint(1,100000))
+# # print(l1)
+#
+# def func1(elem):
+#     if elem > 95000:
+#         return True
+#
+# l2 = filter(func1,l1)
+# print(len(list(l2)))
+# print('%s, eggs, and %s'%('spam','SPAM1'))
